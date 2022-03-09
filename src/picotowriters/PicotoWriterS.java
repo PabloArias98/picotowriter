7c42cc448a244fd831ea4288960845049277535e
Pablo Arias <pabloarias98@hotmail.com>
2022-03-09 17:17:55 +0100

    Añadido directorio de iconos y subidos archivos version 1.1

---
+++ src/picotowriters/PicotoWriterS.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package picotowriters;
+
+/**
+ *
+ * @author Pablo
+ */
+public class PicotoWriterS {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Worder tarea = new Worder();
+        tarea.setVisible(true);
+        
+    }
+    
+}
